7a6a364c412acbf212e7bcef76c7265929907df4
David Dai <18913073128@163.com>
2024-10-07 02:49:22 +0800

    Upgrade: Gradle 8.7

---
+++ app/src/main/java/com/madsam/otora/ui/bof/BofScreen.kt
@@ -0,0 +1,15 @@
+package com.madsam.otora.ui.bof
+
+import androidx.compose.runtime.Composable
+
+/**
+ * 项目名: OtogeTracker
+ * 文件名: com.madsam.otora.ui.bof.BofScreen
+ * 创建者: MadSamurai
+ * 创建时间: 2024/10/7
+ * 描述: TODO
+ */
+@Composable
+fun RecordScreen() {
+
+}
